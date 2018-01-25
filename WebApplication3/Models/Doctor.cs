a3903a716e864e070dd90be9f8783d143a1be438
Damon T Quire <dtquir01@cardmail.louisville.edu>
2018-01-25 12:08:02 -0500

    added more properties

--- WebApplication3/Models/Doctor.cs
+++ WebApplication3/Models/Doctor.cs
@@ -5,7 +5,10 @@ using System.Web;
 
 namespace WebApplication3.Models
 {
-    public class Doctor
+    public class Doctor:Volunteer
     {
+        public string MedicalID { get; set; }
+        public string Specialty { get; set; }
+        public string ReferalCode { get; set; }
     }
 }
