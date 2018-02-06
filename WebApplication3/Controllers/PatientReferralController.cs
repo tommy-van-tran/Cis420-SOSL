7f71bb57a0e7333021aff379c968209948295fff
Damon T Quire <dtquir01@cardmail.louisville.edu>
2018-02-06 15:23:08 -0500

    added patient referral, changed size of font on nav

---
+++ WebApplication3/Controllers/PatientReferralController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class PatientReferralController : Controller
+    {
+        // GET: PatientReferral
+        public ActionResult PatientReferral()
+        {
+            return View();
+        }
+        public ActionResult PatientReferralPasswordPage()
+        {
+            return View();
+        }
+    }
+}
