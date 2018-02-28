22f288f28244eb197cdafd1344ae1a136716d70e
Samuel B Tiefermann <Sam@DESKTOP-GQ99HJP>
2018-02-27 22:12:48 -0500

    LandingPage,UserPrivileges,etc

---
+++ WebApplication3/Controllers/LandingPageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    [Authorize]
+    public class LandingPageController : Controller
+    {
+        // GET: LandingPage
+        public ActionResult LandingPage()
+        {
+            return View();
+        }
+    }
+}
