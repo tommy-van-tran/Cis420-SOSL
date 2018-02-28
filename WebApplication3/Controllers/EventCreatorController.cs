fab4045a3ea96085dc98c3ff7847d2e59d652004
Caleb A Hayden <cahayd03@louisville.edu>
2018-02-28 18:44:04 -0500

    added viewmodel for Event

--- WebApplication3/Controllers/EventCreatorController.cs
+++ WebApplication3/Controllers/EventCreatorController.cs
@@ -8,10 +8,46 @@ namespace WebApplication3.Controllers
 {
     public class EventCreatorController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         // GET: EventCreator
         public ActionResult EventCreator()
         {
             return View();
         }
+    }
+
+
+    public ActionResult EventCreator(EventViewModel viewmodel)
+    {
+        var NonClinical = new NonClinicalModel()
+        {
+            Id = viewmodel.Id,
+            FirstName = viewmodel.FirstName,
+            LastName = viewmodel.LastName,
+            Email = viewmodel.Email,
+            Address = viewmodel.Address,
+            City = viewmodel.City,
+            State = viewmodel.State,
+            PostalCode = viewmodel.PostalCode,
+            PhoneNumber = viewmodel.PhoneNumber,
+            Birthday = viewmodel.Birthday,
+            PreferredName = viewmodel.PreferredName,
+            Employer = viewmodel.Employer,
+            BestDescription = viewmodel.BestDescription,
+            Registration = viewmodel.Registration,
+            Cleaning = viewmodel.Cleaning,
+            ClinicalShadowing = viewmodel.ClinicalShadowing,
+            Fundraising = viewmodel.Fundraising,
+            WhyInterested = viewmodel.WhyInterested,
+            TShirtSize = viewmodel.TShirtSize
+        };
+        db.NonClinicals.Add(NonClinical);
+
+        db.SaveChanges();
+
+
+
+        return RedirectToAction("Index", "Home");
     }
 }
---
+++ WebApplication3/Models/EventViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class EventViewModel
+    {
+        public int ID { get; set; }
+        public string EventSurgery { get; set; }
+        public string EventLocation { get; set; }
+         public DateTime EventDate { get; set; }
+        public ReferAPatientModel Patient { get; set; }
+    }
+}
