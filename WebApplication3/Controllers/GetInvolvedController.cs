d601da7c427946456e9386b5346bc427560ee77a
Brandon McWilliams <brandonmcwilliams1996@gmail.com>
2018-02-27 23:22:33 -0500

    .

--- WebApplication3/Controllers/GetInvolvedController.cs
+++ WebApplication3/Controllers/GetInvolvedController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication3.Models;
 
 namespace WebApplication3.Controllers
 {
     public class GetInvolvedController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         // GET: GetInvolved
         public ActionResult GetInvolved()
         {
@@ -33,6 +35,39 @@ namespace WebApplication3.Controllers
         public ActionResult NonPhysicianClinicalForm()
         {
             return View();
+        }
+
+        public ActionResult NonClinical(NonClinicalViewModel viewmodel)
+        {
+            var NonClinical = new NonClinicalModel()
+            {
+                Id = viewmodel.Id,
+                FirstName = viewmodel.FirstName,
+                LastName = viewmodel.LastName,
+                Email = viewmodel.Email,
+                Address = viewmodel.Address,
+                City = viewmodel.City,
+                State = viewmodel.State,
+                PostalCode = viewmodel.PostalCode,
+                PhoneNumber = viewmodel.PhoneNumber,
+                Birthday = viewmodel.Birthday,
+                PreferredName = viewmodel.PreferredName,
+                Employer = viewmodel.Employer,
+                BestDescription = viewmodel.BestDescription,
+                Registration = viewmodel.Registration,
+                Cleaning = viewmodel.Cleaning,
+                ClinicalShadowing = viewmodel.ClinicalShadowing,
+                Fundraising = viewmodel.Fundraising,
+                WhyInterested = viewmodel.WhyInterested,
+                TShirtSize = viewmodel.TShirtSize
+            };
+            db.NonClinicals.Add(NonClinical);
+
+            db.SaveChanges();
+
+
+
+            return RedirectToAction("Index", "Home");
         }
         public ActionResult VolunteerComment()
         {
--- WebApplication3/Models/NonClinicalModel.cs
+++ WebApplication3/Models/NonClinicalModel.cs
@@ -8,7 +8,7 @@ namespace WebApplication3.Models
 {
     public class NonClinicalModel
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
