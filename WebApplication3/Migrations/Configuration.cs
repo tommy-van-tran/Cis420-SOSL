fa6ccef27b0e2b7072476743907645cf54db8bbe
Caleb A Hayden <cahayd03@louisville.edu>
2018-02-28 23:56:16 -0500

    change config

--- WebApplication3/Migrations/Configuration.cs
+++ WebApplication3/Migrations/Configuration.cs
@@ -10,6 +10,7 @@ namespace WebApplication3.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
             ContextKey = "WebApplication3.Models.ApplicationDbContext";
         }
 
