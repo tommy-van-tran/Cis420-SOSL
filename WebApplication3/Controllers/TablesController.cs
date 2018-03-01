fa18e1a900190a871de44c9cdb725e7fa684cc51
Damon T Quire <dtquir01@louisville.edu>
2018-02-28 21:57:16 -0500

    view databases

--- WebApplication3/Controllers/TablesController.cs
+++ WebApplication3/Controllers/TablesController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication3.Models;
 
 namespace WebApplication3.Controllers
 {
     public class TablesController : Controller
-    {
+    { private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Tables
         public ActionResult Tables()
         {
+            var eventModels = db.EventModels.ToList();
+            ViewBag.EventModels = eventModels;
+            var nonClinical = db.NonClinicals.ToList();
+            ViewBag.NonClinicals = nonClinical;
             return View();
         }
     }
