fd8f7bff39e81fad1b1fe80b08706e9caf05b2b4
Damon T Quire <dtquir01@louisville.edu>
2018-03-07 11:08:59 -0500

    DONE

--- WebApplication3/Controllers/TablesController.cs
+++ WebApplication3/Controllers/TablesController.cs
@@ -12,19 +12,20 @@ namespace WebApplication3.Controllers
         // GET: Tables
         public ActionResult Tables()
         {
-            var eventModels = db.EventModels.ToList();
+            var eventModels = db.EventModels.OrderByDescending<EventModel,int>(e => e.ID).ToList();
+            
             ViewBag.EventModels = eventModels;
 
-            var nonClinical = db.NonClinicals.ToList();
+            var nonClinical = db.NonClinicals.OrderByDescending<NonClinicalModel,int>(e=>e.Id).ToList();
             ViewBag.NonClinicals = nonClinical;
 
-            var nonPhysicianClinical = db.NonPhysicianClinicals.ToList();
+            var nonPhysicianClinical = db.NonPhysicianClinicals.OrderByDescending<NonPhysicianClinicalModel, int>(e => e.ID).ToList();
             ViewBag.NonPhyiscianClinicals = nonPhysicianClinical;
 
-            var physician = db.Physicians.ToList();
+            var physician = db.Physicians.OrderByDescending<PhysicianModel, int>(e => e.Id).ToList();
             ViewBag.Physicians = physician;
 
-            var interpreter = db.MedicalInterpreters.ToList();
+            var interpreter = db.MedicalInterpreters.OrderByDescending<MedicalInterpreterModel, int>(e => e.ID).ToList();
             ViewBag.MedicalInterpreters = interpreter;
             return View();
         }
